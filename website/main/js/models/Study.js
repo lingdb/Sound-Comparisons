ef4d0edb923001ff149c86f518647700ab953eb2
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-08-26 09:40:25 +0200

    Implemented methods get{Name,Title} for Study
    - These methods already depend on dynamic translation.

--- website/main/js/models/Study.js
+++ website/main/js/models/Study.js
@@ -15,4 +15,22 @@ Study = Backbone.Model.extend({
       this.set(data.study);
     }
   }
+/**
+  Returns the name for the current study in the current translation.
+*/
+, getName: function(){
+    var category = 'StudyTranslationProvider'
+      , field    = this.get('Name')
+    return window.App.translationStorage.translateDynamic(category, field, field);
+  }
+/**
+  Returns the title for the current study in the current translation.
+  The title is typically composed with website_title_{prefix,suffix} into the page title.
+  This composition, however should be done in the according view rather than the study.
+*/
+, getTitle: function(){
+    var category = 'StudyTitleTranslationProvider'
+      , field    = this.get('Name')
+    return window.App.translationStorage.translateDynamic(category, field, field);
+  }
 });
