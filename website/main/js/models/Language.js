95918fda49ff74eb0f6aad7823e11448b5c84ad6
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-08-26 15:20:52 +0200

    Implemented rfcLanguage related methods in models/Language.js
    - Missing parts are:
      - Fetching of related Regions
      - Fetching of related Family
      - Family fetching related Languages
      - Fetching name
      - Fetching superscript

--- website/main/js/models/Language.js
+++ website/main/js/models/Language.js
@@ -1,3 +1,47 @@
 /***/
 Language = Backbone.Model.extend({
+  initialize: function(){
+    //Field for memoization of this languages rfcLanguage:
+    this._rfcLanguage = null; // null -> not tried, undefined -> not found.
+    //Field for memoization of languages that have this language as rfcLanguage:
+    this._rfcLanguages = null;
+  }
+  /**
+    Returns the RfcLanguage for the current Language.
+    The field _rfcLanguage is used for memoization.
+  */
+, getRfcLanguage: function(){
+    if(this._rfcLanguage === null){
+      var rfcId = this.get('RfcLanguage');
+      if(typeof(rfcId) === 'string'){
+        var rs = App.languageCollection.where({LanguageIx: rfcId});
+        this._rfcLanguage = rs[0];
+      }else{
+        // We cannot find an RfcLanguage:
+        this._rfcLanguage = undefined;
+      }
+    }
+    return this.rfcLanguage;
+  }
+  /**
+    Returns a Collection of all Languages that have this language as their RfcLanguage.
+  */
+, getRfcLanguages: function(){
+    if(this._rfcLanguages === null){
+      var ls = App.languageCollection.where({RfcLanguage: this.get('LanguageIx')});
+      this._rfcLanguages = new LanguageCollection(ls);
+    }
+    return this._rfcLanguages;
+  }
+  /**
+    Predicate to determine, if this language is the rfcLanguage of another language.
+  */
+, isRfcLanguage: function(){
+    var rls = this.getRfcLanguages();
+    return rls.length > 0;
+  }
+//FIXME relation with region
+//FIXME relation with family
+//FIXME name
+//FIXME superscript
 });
