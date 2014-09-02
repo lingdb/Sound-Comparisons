da3286299803f063644ea333cea2e553d23fb746
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-09-02 15:12:21 +0200

    Added routes to Router
    - there is now a missingRoute route, that I shall use to catch missing link implementations.
    - there are now /*config versions of pageView routes,
      and I want a mechanism, to configure and forward to the simpler routes.

--- website/main/js/Router.js
+++ website/main/js/Router.js
@@ -5,11 +5,20 @@
 Router = Backbone.Router.extend({
   routes: {
     //Basic routes for pageViews:
-    ":study/map/:word/:languages":              "mapView"
-  , ":study/word/:word":                        "wordView"
-  , ":study/language/:language":                "languageView"
-  , ":study/languagesXwords/:languages/:words": "languageWordView"
-  , ":study/wordsXlanguages/:words/:languages": "wordLanguageView"
-  , "*actions":                                 "defaultRoute"
+    ":study/map/:word/:languages":                      "mapView"
+  , ":study/word/:word":                                "wordView"
+  , ":study/language/:language":                        "languageView"
+  , ":study/languagesXwords/:languages/:words":         "languageWordView"
+  , ":study/wordsXlanguages/:words/:languages":         "wordLanguageView"
+    //pageView routes with config info:
+  , ":study/map/:word/:languages/*config":              "mapViewConfig"
+  , ":study/word/:word/*config":                        "wordViewConfig"
+  , ":study/language/:language/*config":                "languageViewConfig"
+  , ":study/languagesXwords/:languages/:words/*config": "languageWordViewConfig"
+  , ":study/wordsXlanguages/:words/:languages/*config": "wordLanguageViewConfig"
+    //Route for missing implementations of links:
+  , "FIXME":                                            "missingRoute"
+    //Catch all route:
+  , "*actions":                                         "defaultRoute"
   }
 });
