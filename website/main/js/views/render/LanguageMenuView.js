368bbe62acdf9c3e56f5112035e61accc0aad691
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-09-04 16:05:35 +0200

    Implemented LanguageMenuView:updateTree
    - buildRegionTree is its helper method

--- website/main/js/views/render/LanguageMenuView.js
+++ website/main/js/views/render/LanguageMenuView.js
@@ -33,7 +33,141 @@ LanguageMenuView = Backbone.View.extend({
   /**
     Builds the complete tree of [families ->] regions -> languages
   */
-, updateTree: function(){}
+, updateTree: function(){
+    if(App.study.getColorByFamily()){
+      var families = [];
+      App.familyCollection.each(function(f){
+        //Checking if we got regions:
+        var regions = f.getRegions();
+        if(regions.length === 0) return;
+        var selected = App.familyCollection.isSelected(f)
+          , data = { // Basic information for a family
+              name:  f.getName()
+            , color: f.getColor()
+            , link:  selected ? 'href="#FIXME/implement removing families"'
+                              : 'href="#FIXME/implement adding families"'
+            , checkbox: {
+                icon: 'icon-chkbox-custom'
+              }
+            };
+        //Checkbox info:
+        var languages = f.getLanguages();
+        switch(App.languageCollection.areSelected(languages)){
+          case 'all':
+            data.checkbox.icon = 'icon-check';
+            data.checkbox.href = 'href="#FIXME/implement removing languages"';
+            data.checkbox.ttip = App.translationStorage.translateStatic('multimenu_tooltip_del_family');
+          break;
+          case 'some':
+            data.checkbox.icon = 'icon-chkbox-half-custom';
+          case 'none':
+            data.checkbox.href = 'href="#FIXME/implement adding languages"';
+            data.checkbox.ttip = App.translationStorage.translateStatic('multimenu_tooltip_add_family');
+        }
+        //The RegionList:
+        if(selected){
+          data.RegionList = this.buildRegionTree(regions);
+        }
+        //Finish:
+        families.push(data);
+      }, this);
+      this.setModel({families: families});
+    }else{
+      this.setModel({RegionList: this.buildRegionTree(App.regionCollection)});
+    }
+  }
+  /**
+    Helperfunction for updateTree that builds a RegionList for a given collection of regions.
+  */
+, buildRegionTree: function(regions){
+    var regionList = {
+      isDl: !App.study.getColorByFamily()
+    , regions: []
+    };
+    regions.each(function(r){
+      var languages = r.getLanguages();
+      if(languages.length === 0)
+        return;
+      var isMultiView = false // FIXME implement
+        , isMapView   = false // FIXME implement
+        , region      = {
+            selected: App.regionCollection.isSelected(r)
+          , name: r.getShortName()
+          , ttip: r.getLongName()
+          , languages: []
+          };
+      //Filling the checkbox:
+      if(isMultiView||isMapView){
+        var box = {icon: 'icon-chkbox-custom'};
+        switch(App.languageCollection.areSelected(languages)){
+          case 'all':
+            box.icon = 'icon-check';
+            box.href = 'href="#FIXME/implement removing languages"';
+            box.ttip = App.translationStorage.translateStatic('multimenu_tooltip_minus');
+          break;
+          case 'some':
+            box.icon = 'icon-chkbox-half-custom';
+          case 'none':
+            box.href = 'href="#FIXME/implement adding languages"';
+            box.ttip = App.translationStorage.translateStatic('multimenu_tooltip_plus');
+        }
+        region.checkbox = box;
+      }
+      //The color:
+      if(regionList.isDl){
+        region.color = r.getColor();
+      }
+      //The link:
+      region.link = region.selected
+                  ? 'href="#FIXME/implement removing regions"'
+                  : 'href="#FIXME/implement adding regions"';
+      //The triangle:
+      region.triangle = region.selected
+                      ? 'icon-chevron-down'
+                      : 'icon-chevron-up rotate90';
+      //Languages for selected Regions:
+      if(region.selected){
+        languages.each(function(l){
+          var language = {
+            shortName: l.getShortName()
+          , longName:  l.getLongName()
+          , selected:  App.languageCollection.isSelected(l)
+          , link:      'href="#FIXME/implement switching to a language"'
+          };
+          //TODO implement flags if wanted!
+          //language.flag = l.getFlag();
+          //Building the icon for a language:
+          if(isMultiView||isMapView){
+            var icon = {
+              checked: language.selected ? 'icon-check' : 'icon-chkbox-custom'
+            , ttip: language.longName+"\n"
+            };
+            if(language.selected){
+              if(isMapView){
+                icon.ttip += App.translationStorage.translateStatic('multimenu_tooltip_del_map');
+              }else{
+                icon.ttip += App.translationStorage.translateStatic('multimenu_tooltip_del');
+              }
+              icon.href = 'href="#FIXME/implement removing a language"';
+            }else{
+              if(isMapView){
+                icon.ttip += App.translationStorage.translateStatic('multimenu_tooltip_add_map');
+              }else{
+                icon.ttip += App.translationStorage.translateStatic('multimenu_tooltip_add');
+              }
+              icon.href = 'href="#FIXME/implement adding a language"';
+            }
+            language.icon = icon;
+          }
+          //Finish:
+          region.languages.push(language);
+        }, this);
+      }
+      //Finish:
+      regionList.regions.push(region);
+    }, this);
+    return regionList;
+  }
 , render: function(){
     console.log('LanguageMenuView.render()');
     this.$el.html(App.templateStorage.render('LanguageMenu', {LanguageMenu: this.model}));
