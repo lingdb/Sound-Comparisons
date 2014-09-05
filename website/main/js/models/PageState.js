d2af46157de934ebc7e77313d256931971b9d5c2
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-09-05 14:33:17 +0200

    PageState now tracks wordOrder

--- website/main/js/models/PageState.js
+++ website/main/js/models/PageState.js
@@ -4,4 +4,33 @@
   - It aids construcing links for the site.
   - It assists in parsing links for the site.
 */
-PageState = Backbone.Model.extend({});
+PageState = Backbone.Model.extend({
+  defaults: {
+    wordOrder: 'alphabetical'
+  }
+//Managing the wordOrder:
+  /**
+    Predicate to test if the wordOrder is logical
+  */
+, wordOrderIsLogical: function(){
+    return this.get('wordOrder') === 'logical';
+  }
+  /**
+    Predicate to test if the wordOrder is alphabetical
+  */
+, wordOrderIsAlphabetical: function(){
+    return this.get('wordOrder') === 'alphabetical';
+  }
+  /**
+    Sets the wordOrder to logical
+  */
+, wordOrderSetLogical: function(){
+    this.set({wordOrder: 'logical'});
+  }
+  /**
+    Sets the wordOrder to alphabetical
+  */
+, wordOrderSetAlphabetical: function(){
+    this.set({wordOrder: 'alphabetical'});
+  }
+});
--- website/main/js/views/render/WordMenuView.js
+++ website/main/js/views/render/WordMenuView.js
@@ -9,17 +9,43 @@ WordMenuView = Backbone.View.extend({
   }
 , activate: function(){
     //Setting callbacks to update model:
-    App.translationStorage.on('change:translationId', function(){
-      App.views.renderer.callUpdates(this);
-    }, this);
+    App.translationStorage.on('change:translationId', this.updateStatic, this);
+    App.pageState.on('change:wordOrder', this.updateSortBy, this);
     //Calling updates:
     App.views.renderer.callUpdates(this);
   }
 , updateStatic: function(){
     var staticT = App.translationStorage.translateStatic({
       title: 'menu_words_words'
+    , sortBy: {
+        sortBy: 'menu_words_selectortext'
+      , aOrder: 'menu_words_selector_rfclangs'
+      , lOrder: 'menu_words_sortelicitation'
+      }
+    , searchFilter: {
+        sfby:             'menu_words_filter_head'
+      , spelling:         'menu_words_filterspelling'
+      , phonetics:        'menu_words_filterphonetics'
+      , soundFile:        'menu_words_filter_fonetics_file'
+      , fonetics:         'menu_words_filter_fonetics'
+      , psTarget:         'menu_words_filter_regex_link'
+      , psHover:          'menu_words_filter_regex_hover'
+      , in:               'menu_words_filter_spphin'
+      , ipaOpenTitle:     'menu_words_open_ipaKeyboard'
+      , filterFoundWords: 'menu_words_filterFoundWords'
+      //FIXME WIP
+      //, soundPath:        => Config::$soundPath
+      }
     });
     this.setModel(staticT);
+  }
+, updateSortBy: function(){
+    var data = {
+      isLogical: App.pageState.wordOrderIsLogical()
+    };
+    data.link = data.isLogical ? 'href="#FIXME/implement setting word order to alphabetical"'
+                               : 'href="#FIXME/implement setting word order to logical"';
+    this.setModel({sortBy: data});
   }
 , render: function(){
     console.log('WordMenuView.render()');
