787aeb7269ea5b30d8d6a82a1484c8851eee0c39
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-09-23 15:53:21 +0200

    Updated StudyWatcher.
    - It appears that displaying the Andean study causes problems.
      Probably the colorByFamily thing isn't tested well enough yet.

--- website/main/js/models/StudyWatcher.js
+++ website/main/js/models/StudyWatcher.js
@@ -1,22 +1,32 @@
 StudyWatcher = Backbone.Model.extend({
-  initialize: function(){
-    //Parsing the current study:
-    var study  = 'Germanic';
-    var sParse = /.*study=([^&]*).*/.exec(window.location.href);
-    if(sParse !== null && sParse.length > 1)
-      study = sParse[1];
-    //Looking for the last study:
-    var lastStudy = study;
-    if(localStorage.lastStudy)
-      lastStudy = localStorage.lastStudy;
+  defaults: {study: null, lastStudy: null}
+, initialize: function(){
+    var s = ls = (l = localStorage.lastStudy) ? l : 'Germanic';
     //The current study will become the last study:
-    localStorage.lastStudy = study;
+    localStorage.lastStudy = s;
     //Setting the vals:
-    this.set({study: study, lastStudy: lastStudy});
+    this.set({study: s, lastStudy: ls});
+  }
+, listen: function(){
+    //We only perform once setup is complete:
+    App.setupBar.onFinish(function(){
+      App.study.on('change', this.update, this);
+      this.update();
+    }, this);
+  }
+  /**
+    Called on complete setup and everytime the study changes.
+  */
+, update: function(){
+    console.log('StudyWatcher.update()');
+    var s  = App.study.getId() || 'Germanic'
+      , ls = localStorage.lastStudy || s;
+    //The current study will become the last study:
+    localStorage.lastStudy = s;
+    //Setting the vals:
+    this.set({study: s, lastStudy: ls});
   }
 , studyChanged: function(){
-    var s  = this.get('study');
-    var ls = this.get('lastStudy');
-    return (s !== ls);
+    return this.get('study') !== this.get('lastStudy');
   }
 });
