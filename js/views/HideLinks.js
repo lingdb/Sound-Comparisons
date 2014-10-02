c42c81bdee837a380ff6a36fb408d04b89928d60
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-10-02 13:43:37 +0200

    Simplified HideLinks
    - HideLinks no longer act on any state in localStorage.
      Having state for HideLinks in localStorage broke the page different times,
      and we don't really need it anyway.
    - Closes #26

--- js/views/HideLinks.js
+++ js/views/HideLinks.js
@@ -2,18 +2,15 @@
 var HideLinks = Backbone.View.extend({
   initialize: function(){
     this.content = $('#contentArea');
-    this.links   = {};
+    this.links   = {}; // name -> jQuery
     var view     = this;
     $('a.hidelink').click(function(){
       view.click($(this));
     }).each(function(){
-      var t   = $(this)
-        , key = t.attr('data-name');
-      view.links[key] = t;
+      var t = $(this)
+        , n = t.data('name');
+      view.links[n] = t;
     });
-    this.loadStates();
-    App.pageState.on('change:pageView', this.render, this);
-    this.render();
   }
 , toggleChevron: function(t){
     if(t.hasClass('icon-chevron-right')){
@@ -47,31 +44,5 @@ var HideLinks = Backbone.View.extend({
     //Telling the MapView to adjust:
     if(window.App.views.mapView)
       window.App.views.mapView.adjustCanvasSize();
-  }
-, saveState: function(t, hidden){
-    var key = this.storageKey(t.data('name'));
-    localStorage[key] = hidden;
-  }
-, loadStates: function(){
-    _.each(this.links, function(l, name){
-      var key = this.storageKey(name);
-      if(localStorage[key] === 'true')
-        l.trigger('click');
-    }, this);
-  }
-, storageKey: function(id){
-    var view = App.pageState.getPageViewKey();
-    return 'HideLinks_toggle_'+view+'_'+id;
-  }
-, render: function(){
-    //Making sure, all are hidden or shown, depending on view:
-    var pv    = App.pageState.getPageViewKey()
-      , hasPv = pv === 'whoAreWe';
-    _.each(this.links, function(l, name){
-      var key = this.storageKey(name);
-      if((!localStorage[key] && hasPv)||(localStorage[key] && !hasPv)){
-        l.trigger('click');
-      }
-    }, this);
   }
 });
