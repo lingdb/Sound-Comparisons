667b2b9911472206bf7354c1765d99e29f7ea09b
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2014-12-04 16:17:25 +0100

    Adjustments to the upcomming sqlFrontend

--- admin/sqlFrontend.php
+++ admin/sqlFrontend.php
@@ -14,32 +14,40 @@ if(!session_mayEdit($dbConnection))
   ?>
   <body>
     <?php require_once 'topmenu.php'; ?>
-    <div class="alert alert-error">
-      <h2>This feature is currently under construction.</h2>
-      You cannot perform SQL operations now, but it will become possible in the future.
-    </div>
-    <form class="form-horizontal" action="query/sql.php?action=import" method="POST" enctype="multipart/form-data">
-      <legend>Upload a .sql file:</legend>
-      <div class="control-group">
-        <label class="control-label" for="import">File to upload:</label>
-        <div class="controls">
-          <input name="import" type="file" required/>
-        </div>
-      </div>
-      <div class="control-group">
-        <div class="controls">
-          <button type="submit" class="btn">Upload</button>
-        </div>
+    <form>
+      <legend>Perform SQL queries:</legend>
+      <textarea>KRAGEN!</textarea>
+      <br>
+      <button type="submit" class="btn btn-danger">Execute!</button>
       </div>
     </form>
-    <form class="form-horizontal">
-      <legend>Export a database dump:</legend>
-      <div class="control-group">
-        <label class="control-label" for="export">Download .sql file:</label>
-        <div class="controls">
-          <a class="btn" href="query/sql.php?action=export">Export</a>
-        </div>
+    <div class="row-fluid">
+      <div class="span6">
+        <form class="form-horizontal" action="query/sql.php?action=import" method="POST" enctype="multipart/form-data">
+          <legend>Upload a .sql file:</legend>
+          <div class="control-group">
+            <label class="control-label" for="import">File to upload:</label>
+            <div class="controls">
+              <input name="import" type="file" required/>
+            </div>
+          </div>
+          <div class="control-group">
+            <div class="controls">
+              <button type="submit" class="btn">Upload</button>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
+      <div class="span6">
+        <form class="form-horizontal">
+          <legend>Export a database dump:</legend>
+          <div class="control-group">
+            <label class="control-label" for="export">Download .sql file:</label>
+            <div class="controls">
+              <a class="btn" href="query/sql.php?action=export">Export</a>
+            </div>
+          </div>
+        </form>
+      </div>
   </body>
 </html>
