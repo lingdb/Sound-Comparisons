dc794e214c6fa991c8a567a1444781d00a467c29
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-05-12 13:42:03 +0200

    Bugfix for missing soundfiles feature
    - The feature now requires the client to choose a study,
      and only shows missing soundfiles for that study.
      This uses fewer resources, and thereby circumvents crashing.
    - Fixes #137

--- admin/missingSounds.php
+++ admin/missingSounds.php
@@ -14,16 +14,24 @@ if(!session_mayEdit($dbConnection))
   ?>
   <body>
     <?php require_once 'topmenu.php'; ?>
+    <h3>Choose a study to list missing files for:</h3>
+    <div class="btn-group">
+    <?php
+      $current = isset($_GET['study']) ? $_GET['study'] : '';
+      foreach(DataProvider::getStudies() as $s){
+        $style = ($s === $current) ? ' btn-inverse' : '';
+        echo "<a class=\"btn$style\" href=\"?study=$s\">$s</a>";
+      }
+    ?>
+    </div>
+    <?php if(isset($_GET['study'])){ ?>
     <table class="display table table-bordered">
       <?php
         $head = '<tr><th>Missing files:</th></tr>';
         echo "<thead>$head</thead>";
         //Making sure we're at the right location:
         chdir('..');
-        //getTranscriptions fills the missingSounds:
-        foreach(DataProvider::getStudies() as $s){
-          DataProvider::getTranscriptions($s);
-        }
+        DataProvider::getTranscriptions($_GET['study']);
         //We use missingSounds to fill the table:
         foreach(DataProvider::$missingSounds as $sound){
           echo "<tr><td>$sound</td></tr>";
@@ -36,5 +44,6 @@ if(!session_mayEdit($dbConnection))
         $('table.display').DataTable({paging: false, ordering: false});
       });
     </script>
+    <?php } ?>
   </body>
 </html>
