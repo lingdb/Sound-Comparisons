4fb7b56e5b3e7e21eaffb9bb08958ad8d0188c8b
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-08-26 16:28:48 +0200

    Bugfix in Family.getRegions()
    - FamilyIds with trailing '0' needed special handling:
      The trailing '0's work as a kind of wildcard that matches everything that is not
      matched by another FamilyId.
    - Example:
        Family 1 has Id '28'
        Family 2 has Id '20'
        -> All Regions where '28' is prefix belong to Family 1
        -> All Regions where '2' is a prefix, but '28' is not, belong to Family 2

--- js/models/Family.js
+++ js/models/Family.js
@@ -37,14 +37,51 @@ define(['backbone','collections/RegionCollection','collections/LanguageCollectio
       return App.translationStorage.translateDynamic(category, field, field);
     }
     /**
+      @return regions RegionCollection
       Returns a collection of all regions that belong to this family.
+      Finding belonging regions turns out to be kind of tricky.
+      Example:
+        Family 1 has Id '28'
+        Family 2 has Id '20'
+        -> All Regions where '28' is prefix belong to Family 1
+        -> All Regions where '2' is a prefix, but '28' is not, belong to Family 2
     */
   , getRegions: function(){
       if(!this._regions){
-        var familyId = this.getId()
-          , regions  = App.regionCollection.filter(function(r){
-            return r.getId().indexOf(familyId) === 0;
+        var familyId = this.getId()//Current familyId
+          , regions = [];//[Region]
+        //Testing if familyId has a 0 suffix:
+        var matches = familyId.match(/^([^0]+)0+$/);
+        if(matches !== null){
+          //Prefix case:
+          //Searching other Ids:
+          var otherIds = [];
+          App.familyCollection.each(function(f){
+            var fId = f.getId();
+            if(fId !== familyId){
+              otherIds.push(fId);
+            }
           });
+          //Considering only prefix as current familyId:
+          familyId = matches[1];
+          //Selecting regions:
+          regions = App.regionCollection.filter(function(r){
+            var rId = r.getId();
+            //Checking that otherIds don't contain a prefix for rId:
+            var found = _.any(otherIds, function(oId){
+              return rId.indexOf(oId) === 0;
+            });
+            if(found) return false;
+            //Checking that familyId is a prefix for rId:
+            return rId.indexOf(familyId) === 0;
+          });
+        }else{
+          //Simple case:
+          regions = App.regionCollection.filter(function(r){
+              return r.getId().indexOf(familyId) === 0;
+            });
+        }
+        //Setting RegionCollection:
         this._regions = new RegionCollection(regions);
       }
       return this._regions;
