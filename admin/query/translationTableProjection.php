9d4f4f1b971bf4c4c00f575cc2d3986c595c5238
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-09-25 14:19:00 +0200

    Adding TranslationTableProjection.update()
    - Making the TranslationProvider.update() method usable for TranslationTableProjection.
    - Other methods shall soon follow.

--- admin/query/translationTableProjection.php
+++ admin/query/translationTableProjection.php
@@ -189,4 +189,47 @@ class TranslationTableProjection {
     }
     return $projections;
   }
+  /**
+    @param $tId TranslationId
+    @param $payload String fieldSelect value from description
+    @param $update String new value for translation entry
+    @return $this TranslationTableProjection || Exception
+    Build after the preimage of TranslationProvider.update(…)
+    This method updates the translation for a TranslationTableProjection on a single column.
+    Should the projection have more than a single column,
+    an Exception will be returned.
+    All given parameters will be escaped by this method.
+  */
+  public function update($tId, $payload, $update){
+    //Check if projection is on single column:
+    if(count($this->descriptions) !== 1){
+      return new Exception('Projection on other than a single table in TranslationTableProjection.update()');
+    }
+    foreach($this->descriptions as $tableName as $desc){
+      if(count($desc['columns']) !== 1){
+        return new Exception('Projection on other than a single column in TranslationTableProjection.update()');
+      }
+      foreach($desc['columns'] as $column){
+        //Sanitize input:
+        $db       = Config::getConnection();
+        $tId      = $db->escape_string($tId);
+        $payload  = $db->escape_string($payload);
+        $update   = $db->escape_string($update);
+        $category = $column['category'];
+        $qs = array(
+          "DELETE FROM Page_DynamicTranslation "
+        . "WHERE TranslationId = $tId "
+        . "AND Category = '$category' "
+        . "AND Field = '$payload'"
+        , "INSERT INTO Page_DynamicTranslation (TranslationId, Category, Field, Trans) "
+        . "VALUES ($tId, '$category', '$payload', '$update')"
+        , "UPDATE Page_Translations SET lastChangeDynamic = CURRENT_TIMESTAMP() WHERE TranslationId = $tId"
+        );
+        foreach($qs as $q){
+          $db->query($q);
+        }
+      }
+    }
+    return $this;
+  }
 }
