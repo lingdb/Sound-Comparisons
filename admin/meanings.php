efcc81f941e4cb4ba2860ac702ca6d77085e195b
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-10-06 12:30:59 +0200

    Improve Meanings feature
    - Closes #240
    - Relates to db repo c0dbd23b1a5255a98108c828d7a8ff3adee40b6d
    - Now there is an initial table that lists meanings by name and descriptions.
      From there it is possible to edit a meaning and also view the corresponding
      example and justification fields as well as adding a new meaning.

--- admin/meanings.php
+++ admin/meanings.php
@@ -2,10 +2,11 @@
   require_once('common.php');
   if(!session_validate($dbConnection)){
     header('LOCATION: index.php');
-  }else{
+    die();
+  }
   //Checking if some data where posted:
   $hasParams = function(){
-    $params = array('name','description','example');
+    $params = array('name','description','example','justification');
     foreach($params as $p){
       if(!array_key_exists($p, $_POST)){
         return false;
@@ -15,14 +16,15 @@
   };
   //Adding/updating on post:
   if($hasParams()){
-    $q = 'INSERT INTO Meanings (name, description, example) '
-       . 'VALUES (?,?,?) '
-       . 'ON DUPLICATE KEY UPDATE description=?, example=?';
+    $q = 'INSERT INTO Meanings (name, description, example, justification) '
+       . 'VALUES (?,?,?,?) '
+       . 'ON DUPLICATE KEY UPDATE description=?, example=?, justification=?';
     $name = $_POST['name'];
     $description = $_POST['description'];
     $example = $_POST['example'];
+    $justification = $_POST['justification'];
     $stmt = Config::getConnection()->prepare($q);
-    $stmt->bind_param('sssss', $name, $description, $example, $description, $example);
+    $stmt->bind_param('sssssss', $name, $description, $example, $justification, $description, $example, $justification);
     $stmt->execute();
     $stmt->close();
     //Redirect to make sure get rather than post will be used:
@@ -37,43 +39,61 @@
     require_once('head.php');
   ?>
   <body>
-    <?php require_once('topmenu.php'); ?>
+    <?php require_once('topmenu.php');
+      if(array_key_exists('name', $_GET)){
+        $q = 'SELECT name, description, example, justification FROM Meanings WHERE name = ?';
+        $stmt = Config::getConnection()->prepare($q);
+        $stmt->bind_param('s', $_GET['name']);
+        $stmt->execute();
+        $stmt->bind_result($name, $description, $example, $justification);
+        if($stmt->fetch()){?>
+          <form action="index.php?action=meanings" method="post">
+            <fieldset>
+              <legend>Editing Meaning</legend>
+              <input name="name" value="<?php echo $name; ?>" placeholder="New name" type="text" required disabled>
+              <label>Description:</label>
+              <textarea style="width: 100%; height: 250px;" name="description" placeholder="New description" type="text" required><?php echo $description; ?></textarea>
+              <label>Example:</label>
+              <textarea style="width: 100%; height: 250px;" name="example" placeholder="New example" type="text" required><?php echo $example; ?></textarea>
+              <label>Justification:</label>
+              <textarea style="width: 100%; height: 250px;" name="justification" placeholder="New justification" type="text" required><?php echo $justification; ?></textarea>
+              <button type="submit" class="btn">Save</button>
+            </fieldset>
+          </form>
+        <?php }else{
+          echo '<h1>Meaning not found :(</h1>';
+        }
+        $stmt->close();
+      }else{
+    ?>
     <table class="table table-bordered">
       <thead>
         <tr>
           <th>Name:</th>
           <th>Description:</th>
-          <th>Example:</th>
           <th>Action:</th>
         </tr>
       </thead>
       <tbody><?php
-        $q = 'SELECT name, description, example FROM Meanings';
+        $q = 'SELECT name, description FROM Meanings';
         $meanings = DataProvider::fetchAll($q);
         foreach($meanings as $meaning){
           $name = $meaning['name'];
           $description = $meaning['description'];
-          $example = $meaning['example'];
-          $mkInput = function($name, $content){
-            return '<input name="'.$name.'" value="'.$content.'" type="text" required>';
-          };
-          $mkTextarea = function($name, $content){
-            return '<textarea name="'.$name.'" required>'.$content.'</textarea>';
-          };
-          echo '<tr><form action="index.php?action=meanings" method="post">'
-             . '<td>'.$mkInput('name',$name).'</td>'
-             . '<td>'.$mkTextarea('description',$description).'</td>'
-             . '<td>'.$mkTextarea('example',$example).'</td>'
-             . '<td><button type="submit" class="btn">Save</button></td>'
-             . "</form></tr>";
+          echo '<tr>'
+             . "<td>$name</td>"
+             . "<td>$description</td>"
+             . "<td><a href='index.php?action=meanings&name=$name' class='btn'>Edit</a></td>"
+             . '</tr>';
         }?>
         <tr><form action="index.php?action=meanings" method="post">
           <td><input name="name" value="" placeholder="New name" type="text" required></td>
           <td><textarea name="description" value="" placeholder="New description" type="text" required></textarea></td>
-          <td><textarea name="example" value="" placeholder="New example" type="text" required></textarea></td>
           <td><button type="submit" class="btn">Save</button></td>
+          <input name="example" value="New Example" type="hidden">
+          <input name="justification" value="New Justification" type="hidden">
         </form></tr>
       </tbody>
-    </table>
+    </table><?php } /*else*/ ?>
   </body>
-</html><?php }
+</html>
