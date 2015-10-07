29a53126c92b4259cf7010334e56a08c330a22db
Jakob Runge <sicarius@g4t3.de>
2015-10-07 09:55:42 +0200

    Attach templateInfo at correct routes
    - templateInfo is now provided at the routes where the current soundcomparisons presentation code expects it.

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+
+import templateInfo
+
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+templateInfo.addRoutes(app,'/query/templateInfo','/templates/')
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
--- templateInfo.py
+++ templateInfo.py
@@ -1,24 +1,25 @@
 '''
-  Tries to reproduce the thing the site currently does when fetching
-  /query/templateInfo
-  which is delivering a JSON object mapping all mustache templates to their md5 hashes.
-  @author JR
+    Tries to reproduce the thing the site currently does when fetching
+    /query/templateInfo
+    which is delivering a JSON object mapping all mustache templates to their md5 hashes.
+    @author JR
 '''
 
 import os
 import hashlib
+import flask
 
 '''
-  Relative location of mustache templates
+    Relative location of mustache templates
 '''
 def getMustacheDir():
-  return 'static/mustache/'
+    return 'static/mustache/'
 
 '''
-  @param fname String
-  @return sum String
-  Compute the md5sum of a file
-  by reading it into memory in chunks of 4096 bytes.
+    @param fname String
+    @return sum String
+    Compute the md5sum of a file
+    by reading it into memory in chunks of 4096 bytes.
 '''
 def md5(fname):
     hash = hashlib.md5()
@@ -28,18 +29,34 @@ def md5(fname):
     return hash.hexdigest()
 
 '''
-  @param route String
-  @return dict(route+fileName => md5(filename))
-  Iterates all files in mustacheDir and creates a dict
-  that maps route+fileName to the md5sum of the file at getMustacheDir()+fileName
+      @param route String
+      @return dict(route+fileName => md5(filename))
+      Iterates all files in mustacheDir and creates a dict
+      that maps route+fileName to the md5sum of the file at getMustacheDir()+fileName
 '''
 def getTemplateInfo(route):
-  templateMap = {}
-  for fn in os.listdir(getMustacheDir()):
-    file = getMustacheDir()+fn
-    templateMap[route+fn] = md5(file)
-  return templateMap
+    templateMap = {}
+    for fn in os.listdir(getMustacheDir()):
+        file = getMustacheDir()+fn
+        templateMap[route+fn] = md5(file)
+    return templateMap
+
+'''
+    @param app instance of Flask
+    @param queryRoute String
+    @param templateRoute String
+    Attaches templateInfo logic to queryRoute providing templates at templateRoute.
+'''
+def addRoutes(app, queryRoute, templateRoute):
+    @app.route(queryRoute)
+    def returnTemplateInfo():
+        tInfo = getTemplateInfo(templateRoute)
+        return flask.jsonify(**tInfo)
+
+    @app.route(templateRoute+'<path:path>')
+    def returnTemplate(path):
+        return flask.send_from_directory(getMustacheDir(), path)
 
 # Produce test output:
 if __name__ == "__main__":
-  print(getTemplateInfo(getMustacheDir()))
+    print(getTemplateInfo(getMustacheDir()))
