645e64385998ce94fcf3cfdcb1d3e1dccf33652f
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-10-12 17:52:43 +0200

    Implement translationInfo.py
    - Two FIXMEs remain for deprecation cases.
      I intend to check these once the JavaScript side works with flask,
      and remove them if the dependency is indeed resolved.

--- translationInfo.py
+++ translationInfo.py
@@ -5,10 +5,14 @@
 
 import flask
 import sqlalchemy
+from collections import defaultdict
 
 import db
 
-#FIXME DESCRIBE
+'''
+    @return flask reply
+    Provides a JSON encoded summary of available translations to the client.
+'''
 def getSummary():
     tMap = {}#TranslationId -> Page_Translations.toDict()
     ts = db.getSession().query(db.Page_Translations).filter(
@@ -20,47 +24,103 @@ def getSummary():
         tMap[str(t.TranslationId)] = t.toDict()
     return flask.jsonify(**tMap)
 
-#FIXME DESCRIBE
-def getStatic():
-    #FIXME IMPLEMENT
-    return 'getStatic'
+'''
+    @param func Page_Translation -> flask reply
+    @return flask reply
+    Check the existence of a translationId parameter.
+    If the parameter exists and the corresponding instance of Page_Translations
+    can be fetched, the given func is called and it's result returned.
+    Otherwise a JSON encoded error message is returned.
+'''
+def chkTranslationId(func):
+    if 'translationId' in flask.request.args:
+        tId = flask.request.args['translationId']
+        try:
+            t = db.getSession().query(db.Page_Translations).filter_by(TranslationId = tId).limit(1).one()
+            return func(t)
+        except sqlalchemy.orm.exc.NoResultFound:
+            return flask.jsonify(**{
+                'msg': 'Specified translationId not found in database: '+tId
+            })
+    action = flask.request.args['action']
+    return flask.jsonify(**{
+        'msg': 'You need to specify a translationId for action='+action+'.'
+        })
 
-#FIXME DESCRIBE
-def getDynamic():
-    #FIXME IMPLEMENT
-    return 'getDynamic'
+'''
+    @param translation Page_Translations
+    @return flask reply
+    @deprecated since i18n this most likely isn't used any more.
+    Returns JSON encoded data for dynamic translation.
+'''
+# FIXME remove due to deprecation at once.
+def getStatic(translation):
+    static = {}
+    for s in translation.Page_StaticTranslation:
+        static[s.Req] = s.Trans
+    print static
+    return flask.jsonify(**static)
 
-#FIXME DESCRIBE
-def getI18n():
-    #FIXME IMPLEMENT
-    return 'getI18n'
+'''
+    @param translation Page_Translations
+    @return flask reply
+    @deprecated since i18n this most likely isn't used any more.
+    Returns JSON encoded data for dynamic translation.
+    This implementation varies from the website PHP one.
+'''
+# FIXME remove due to deprecation at once.
+def getDynamic(translation):
+    dynamic = defaultdict(lambda: defaultdict(dict))
+    for d in translation.Page_DynamicTranslation:
+        dynamic[d.Category][d.Field] = d.Trans
+    return flask.jsonify(**dynamic)
+
+'''
+    @param lngs [String]
+    @return flask reply
+    Returns the JSON encoded data for translation that can be consumed by i18n client side.
+'''
+def getI18n(lngs):
+    i18n = defaultdict(dict)
+    for l in lngs:
+        try:
+            translation = db.getSession().query(db.Page_Translations).filter_by(BrowserMatch = l).limit(1).one()
+            tDict = {}
+            for s in translation.Page_StaticTranslation:
+                tDict[s.Req] = s.Trans
+            for d in translation.Page_DynamicTranslation:
+                tDict[d.Category+d.Field] = d.Trans
+            i18n[l]['translation'] = tDict
+        except sqlalchemy.orm.exc.NoResultFound:
+            pass
+    return flask.jsonify(**i18n)
 
 '''
     @param app instance of Flask
     Installs the translationInfo module to the /query/translations route,
     from where it serves GET requests.
     It accepts parameters:
-    * summary
-    * static
-    * dynamic
-    * i18n
+    * action in {summary,static,dynamic}
+    * lng = ' '.join([String])
 '''
 def addRoute(app):
     @app.route('/query/translations')
     def getTranslations():
         jumpMap = {
             'summary': getSummary,
-            'static': getStatic,
-            'dynamic': getDynamic,
-            'i18n': getI18n
+            'static':  lambda: chkTranslationId(getStatic),
+            'dynamic': lambda: chkTranslationId(getDynamic),
             }
         #Executing specified action, iff possible:
         if 'action' in flask.request.args:
             action = flask.request.args['action']
             if action in jumpMap:
                 return jumpMap[action]()
+        if 'lng' in flask.request.args:
+            lngs = flask.request.args['lng'].split(' ')
+            return getI18n(lngs)
         #Fallback in case of error:
         return flask.jsonify(**{
-            'msg': '"action" parameter must be specified, carrying one of the action values.',
+            'msg': '"action" parameter must be specified, carrying one of the action values, or a lng parameter must be given.',
             'action': jumpMap.keys()
             })
