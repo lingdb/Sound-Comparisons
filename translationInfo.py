a91575caea37980441b8b10555a3c5f4fe3e3aee
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-10-12 15:57:16 +0200

    Implement summary case in translationInfo.py

--- translationInfo.py
+++ translationInfo.py
@@ -3,6 +3,38 @@
     This module aims to provide translation data in a JSON encoded format.
 '''
 
+import flask
+import sqlalchemy
+
+import db
+
+#FIXME DESCRIBE
+def getSummary():
+    tMap = {}#TranslationId -> Page_Translations.toDict()
+    ts = db.getSession().query(db.Page_Translations).filter(
+            sqlalchemy.or_(
+            db.Page_Translations.Active == 1,
+            db.Page_Translations.TranslationId == 1)
+        ).all()
+    for t in ts:
+        tMap[str(t.TranslationId)] = t.toDict()
+    return flask.jsonify(**tMap)
+
+#FIXME DESCRIBE
+def getStatic():
+    #FIXME IMPLEMENT
+    return 'getStatic'
+
+#FIXME DESCRIBE
+def getDynamic():
+    #FIXME IMPLEMENT
+    return 'getDynamic'
+
+#FIXME DESCRIBE
+def getI18n():
+    #FIXME IMPLEMENT
+    return 'getI18n'
+
 '''
     @param app instance of Flask
     Installs the translationInfo module to the /query/translations route,
@@ -14,4 +46,21 @@
     * i18n
 '''
 def addRoute(app):
-    pass
+    @app.route('/query/translations')
+    def getTranslations():
+        jumpMap = {
+            'summary': getSummary,
+            'static': getStatic,
+            'dynamic': getDynamic,
+            'i18n': getI18n
+            }
+        #Executing specified action, iff possible:
+        if 'action' in flask.request.args:
+            action = flask.request.args['action']
+            if action in jumpMap:
+                return jumpMap[action]()
+        #Fallback in case of error:
+        return flask.jsonify(**{
+            'msg': '"action" parameter must be specified, carrying one of the action values.',
+            'action': jumpMap.keys()
+            })
