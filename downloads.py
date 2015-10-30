fa0bc7a110545c8019e5c65e5424077b61959359
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-10-30 13:58:47 +0100

    Reimplementing downloads feature
    - Starts work on #249

---
+++ downloads.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+    Provide the download endpoints given by code in php/export/*php files.
+'''
+
+'''
+    Build after the preimage of php/export/singleSoundFile.php
+'''
+def singleSoundFile():
+    pass
+
+'''
+    Build after the preimage of php/export/singleTextFile.php
+'''
+def singleTextFile():
+    pass
+
+'''
+    Build after the preimage of php/export/csv.php
+'''
+def buildCSV():
+    # FIXME check where this is used!
+    pass
