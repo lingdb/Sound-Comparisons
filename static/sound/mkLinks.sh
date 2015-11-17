c74b1aabeebcb8faf45c39a0576599066203eb0a
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-17 15:25:44 +0100

    Bugfix in static/sound/mkLinks.sh

--- static/sound/mkLinks.sh
+++ static/sound/mkLinks.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-srcDir="../../../docker.sndcomp/sound/"
+# chdir to location of mkLinks.sh:
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+# Inspecting source directory:
+srcDir="../../../container/sndcomp/sound"
 sources=$(ls $srcDir)
-for s in $sources
-do
-ln -s $srcDir/$s $s
+# Creating softlinks:
+for s in $sources; do
+    ln -s $srcDir/$s $s
 done
