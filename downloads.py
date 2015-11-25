35e4e1b10b8726bef3757e9b5fb43f6c661a79fa
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-25 10:23:43 +0100

    Improve downloads.py singleSoundFile()
    - Relates to #249

--- downloads.py
+++ downloads.py
@@ -5,20 +5,37 @@
 
 import flask
 import re
+import os.path
+import base64
 
 '''
     Build after the preimage of php/export/singleSoundFile.php
 '''
 def singleSoundFile():
     # Making sure file parameter exists:
-    if 'file' in flask.request.args:
-        file = flask.request.args['file']
+    args = flask.request.args
+    if 'file' in args:
+        file = args['file']
         # Preventing path traversal:
         test1 = re.match('\.\.', file)
         test2 = re.match('static/.*', file)
-        if test2 != None and test1 == None:
-            # Instead of the old download we should be able to simply redirect to the static file.
-            return flask.redirect(file, code=302)
+        test3 = re.match('\/\/', file)
+        if test2 != None and test1 == None and test3 == None:
+            # Modeled after https://stackoverflow.com/a/23354496/448591
+            # this is not as fast as redirecting, but does the job.
+            # Code to redirect:
+            # return flask.redirect(file, code=302)
+            filename = os.path.basename(file)
+            headers = {"Content-Disposition": "attachment; filename=%s" % filename}
+            with open(file, 'r') as f:
+                body = f.read()
+                # Encode base64 iff requested:
+                if 'base64' in args:
+                    body = base64.standard_b64encode(body)
+            # Build and return response:
+            response = flask.make_response(body)
+            response.headers["Content-Disposition"] = "attachment; filename=%s" % filename
+            return response
         else:
             return 'Sorry, I cannot serve this file.', 403
     else:
