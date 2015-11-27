5c60c6b0e1c7eeb1bd0e1b6994eb5097c988d3e2
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-27 14:46:42 +0100

    Remove static/{,sound}/mkLinks.sh
    - These have been specific to my setup and don't need to stay.
      They did create some softlinks so I wouldn't need to move sound files around,
      but that's not useful for the project.

--- soundcomparisons/static/mkLinks.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-ln -s ../../website/templates mustache
-ln -s ../../website/img       img
-ln -s ../../website/css       css
-ln -s ../../website/js        js
-ln -s ../../website/sound     sound
