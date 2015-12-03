14ae40290ba6af796b8665817c11ecf0041e0194
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-12-03 14:35:48 +0100

    Add importCSV.py
    - Starting work on #253.

---
+++ soundcomparisons/importCSV.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+    In the php version of the soundcomparisons websites
+    it was possible to import .csv files from Pauls database.
+    This module aims to replicate this feature.
+    For the corresponding .php file
+    a look at admin/query/dbimport/Importer.php may be educational.
+'''
