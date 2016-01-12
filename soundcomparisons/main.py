19079c8597e917b6be3dcc028aef38932263a593
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-01-12 15:47:10 +0100

    Add Alembic via Flask-Migrate
    - I think Robert mentioned that Alembic would be a useful choice to develop the datbase schema.
      To do this in the future I've added the necessary module and modified main.py to use it.
    - However I'm currently not making use of the migrations as there are some parts of the database
      that cannot be tracked by Alembic correctly as they are not even encoded in models.
      I don't want these parts to be dropped on accident and therefore won't make use of migrations just now.

--- soundcomparisons/main.py
+++ soundcomparisons/main.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 """
-run this file to start the app for development
+run this file to start the app for development or to manage migrations.
+See https://flask-migrate.readthedocs.org/en/latest/
 """
-from soundcomparisons import app
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
+from soundcomparisons import app
+from db import db
+
+app.host = '127.0.0.1'
+app.port = 5000
+app.debug = True
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    manager.run()
