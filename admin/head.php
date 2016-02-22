564ccc64b91330a40e7b6702834415dbf63e0bb9
Jakob Runge <sicarius@g4t3.de>
2016-02-22 13:48:48 +0100

    Add favicon to admin area
    - Closes #294

--- admin/head.php
+++ admin/head.php
@@ -6,6 +6,10 @@
       echo 'Beware of magic.';
   ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <!-- For IE 9 and below. ICO should be 32x32 pixels in size -->
+  <!--[if IE]><link rel="shortcut icon" href="../img/favicon_admin.ico"><![endif]-->
+  <link rel="icon" href="../img/favicon_admin.png" type="image/png" sizes="16x16">
+  <link rel="icon" href="../img/favicon_admin.gif" type="image/gif" sizes="16x16">
   <link rel="Stylesheet" type="text/css" href="../css/extern/bootstrap.css" media="screen" />
   <link rel="Stylesheet" type="text/css" href="css/style.css" media="screen" />
   <link rel="Stylesheet" type="text/css" href="css/extern/jquery.dataTables.css" media="screen" />
