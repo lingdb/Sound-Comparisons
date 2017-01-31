59d40c9fde495cb09b1857cb4f7cad34c0ef1dcb
Jakob Runge <runjak@gmail.com>
2017-01-31 14:12:00 +0100

    Fix JavaScript includes in admin/head.php

--- site/admin/head.php
+++ site/admin/head.php
@@ -13,10 +13,10 @@
   <link rel="Stylesheet" type="text/css" href="../css/extern/bootstrap.css" media="screen" />
   <link rel="Stylesheet" type="text/css" href="css/style.css" media="screen" />
   <link rel="Stylesheet" type="text/css" href="css/extern/jquery.dataTables.css" media="screen" />
-  <script type='application/javascript' src='../js/extern/jquery.min.js'></script>
+  <script type='application/javascript' src='../js/bower_components/jquery/dist/jquery.min.js'></script>
   <script type='application/javascript' src='../js/extern/bootstrap.js'></script>
-  <script type='application/javascript' src='../js/extern/underscore-min.js'></script>
-  <script type='application/javascript' src='../js/extern/backbone-min.js'></script>
+  <script type='application/javascript' src='../js/bower_components/underscore/underscore-min.js'></script>
+  <script type='application/javascript' src='../js/bower_components/backbone/backbone-min.js'></script>
   <?php
     if(isset($jsFiles)){
       foreach($jsFiles as $f){
