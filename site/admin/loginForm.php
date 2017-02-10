b25828aa0c995b56d40366829a33a1f33e658251
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2017-02-10 14:07:39 +0100

    set autofocus on first input field for connivence

--- site/admin/loginForm.php
+++ site/admin/loginForm.php
@@ -5,7 +5,7 @@
 ?><div class="container">
   <form class="form-signin" method="post" action="index.php?action=login">
     <h2 class="form-signin-heading"><?php echo $loginMessage; ?></h2>
-    <input name="username" type="text" class="input-block-level" placeholder="Username">
+    <input name="username" type="text" class="input-block-level" placeholder="Username" autofocus>
     <input name="password" type="password" class="input-block-level" placeholder="Password">
     <button class="btn btn-large btn-primary" type="submit">Login</button>
   </form>
