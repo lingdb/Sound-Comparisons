d71fbb64ba57418058fffbb83a63632aaf4b4b19
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2018-04-04 11:55:13 +0200

    fixed translation stats and show all not yet translated entries #477 [4]

--- site/admin/translation/showTable.php
+++ site/admin/translation/showTable.php
@@ -18,50 +18,52 @@ function showTable($tdata, $showKeep = false){
       //Value to echo as row:
       $value = array_shift($field);
       $orig = $value['Original'];
-      if(strlen($orig) === 0) continue;
-      echo "<tr>";
-      //Description:
-      $desc = $value['Description'];
-      $req  = array_key_exists('Req', $desc) ? $desc['Req'] : '';
-      $desc = array_key_exists('Description', $desc) ? $desc['Description'] : '';
-      //Shortening:
-      $maxLen = 12;
-      $short = preg_replace('/<[^<>]+>/','',$desc);
-      if(strlen($short) > $maxLen){$short = substr($short, 0, $maxLen).'…';}
-      //Encoding tooltip contents:
-      $desc = preg_replace("/'/",'"',$desc);
-      $desc = htmlspecialchars($desc);
-      echo "<td class='description'$isAdmin' data-req='$req' data-html='true' data-container='body' data-title='$desc' style='white-space: nowrap;'>$short</td>";
-      //Title in case of search:
-      $title = '';
-      if(array_key_exists('Match', $value)){
-        if($value['Match'] !== $value['Original'] && $value['Match'] !== $value['Translation']){
-          $title = ' title="'.$value['Match'].'"';
+      // show only entries whose 'Original' fields are set
+      if(strlen(trim($orig)) > 0){
+        echo "<tr>";
+        //Description:
+        $desc = $value['Description'];
+        $req  = array_key_exists('Req', $desc) ? $desc['Req'] : '';
+        $desc = array_key_exists('Description', $desc) ? $desc['Description'] : '';
+        //Shortening:
+        $maxLen = 12;
+        $short = preg_replace('/<[^<>]+>/','',$desc);
+        if(strlen($short) > $maxLen){$short = substr($short, 0, $maxLen).'…';}
+        //Encoding tooltip contents:
+        $desc = preg_replace("/'/",'"',$desc);
+        $desc = htmlspecialchars($desc);
+        echo "<td class='description'$isAdmin' data-req='$req' data-html='true' data-container='body' data-title='$desc' style='white-space: nowrap;'>$short</td>";
+        //Title in case of search:
+        $title = '';
+        if(array_key_exists('Match', $value)){
+          if($value['Match'] !== $value['Original'] && $value['Match'] !== $value['Translation']){
+            $title = ' title="'.$value['Match'].'"';
+          }
         }
+        //Original:
+        if(array_key_exists('Study', $value)){//Study in case of search
+          $stud = $value['Study'];
+          $orig = "$stud:<span style='color:red'>$orig</span>";
+        }else{$orig = "<span style='color:red'>$orig</span>";}
+        echo '<td class="original"'.$title.'>'.$orig
+           . '<a class="btn btn-small pull-right copy-over">'
+           . '<i class="icon-arrow-right"></i></a></td>';
+        //Translation:
+        $trans = $value['Translation'];
+        $prov  = $trans['TranslationProvider'];
+        $tId   = $trans['TranslationId'];
+        $pay   = $trans['Payload'];
+        $trans = $trans['Translation'];
+        echo "<td data-tId='$tId' data-provider='$prov' data-payload='$pay'>"
+           . "<input type='text' value='$trans' class='translation' style='width:90%'>"
+           . '<a class="btn btn-small save"><i class="icon-hdd"></i>Save</a>'
+           . $showKeep . '</td>';
+        echo "</tr>";
       }
-      //Original:
-      if(array_key_exists('Study', $value)){//Study in case of search
-        $stud = $value['Study'];
-        $orig = "$stud:<span style='color:red'>$orig</span>";
-      }else{$orig = "<span style='color:red'>$orig</span>";}
-      echo '<td class="original"'.$title.'>'.$orig
-         . '<a class="btn btn-small pull-right copy-over">'
-         . '<i class="icon-arrow-right"></i></a></td>';
-      //Translation:
-      $trans = $value['Translation'];
-      $prov  = $trans['TranslationProvider'];
-      $tId   = $trans['TranslationId'];
-      $pay   = $trans['Payload'];
-      $trans = $trans['Translation'];
-      echo "<td data-tId='$tId' data-provider='$prov' data-payload='$pay'>"
-         . "<input type='text' value='$trans' class='translation' style='width:90%'>"
-         . '<a class="btn btn-small save"><i class="icon-hdd"></i>Save</a>'
-         . $showKeep . '</td>';
       //Handling the exit condition:
       if(count($field) > 0){
         $newTZ[$key] = $field;
       }
-      echo "</tr>";
     }
     $tdata = $newTZ;
   }
