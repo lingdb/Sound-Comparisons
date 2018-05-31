628bb65fe893591025af91faf84c292da5ab84cb
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2018-05-31 21:01:57 +0200

    fixed racing while changing the site language and reintroduced a better way of changing the site language by using pure JS instead of an URL; made generating of this.model of HomeView more consistent

--- site/js/views/render/HomeView.js
+++ site/js/views/render/HomeView.js
@@ -2,34 +2,51 @@
 define(['views/render/SubView', 'models/Loader', 'jquery'],
        function(SubView, Loader, $){
   return SubView.extend({
-    /***/
-    initialize: function(){
-
-      this.model = {
-        studies: this.getAllStudyData
-      , siteLanguage: function(){return App.translationStorage.getBrowserMatch();}
-      , title: function(){return App.translationStorage.translateStatic('website_title_prefix');}
-      , subtitle: function(){return App.translationStorage.translateStatic('website_title_suffix');}
-      , funding: function(){return App.translationStorage.translateStatic('website_logo_hover');}
-      , imprint: function(){return App.translationStorage.translateStatic('topmenu_about_imprint');}
-      , imprintHref: function(){return App.translationStorage.translateStatic('topmenu_about_imprint_href');}
-      , privacypolicy: function(){return App.translationStorage.translateStatic('topmenu_about_privacypolicy');}
-      , privacypolicyHref: function(){return App.translationStorage.translateStatic('topmenu_about_privacypolicy_href');}
-      , licenceText: function(){return App.translationStorage.translateStatic('topmenu_about_licencetext');}
-      , licenceTextHref: function(){return App.translationStorage.translateStatic('topmenu_about_licencetext_href');}
-      , currentFlag: function(){return App.translationStorage.getFlag();}
-      , otherTranslations: function(){return _.chain(App.translationStorage.getOthers()).map(function(tId){
-          return {
-            tId: tId
-          , flag: this.getFlag(tId)
-          , name: this.getName(tId)
-          };
-        }, App.translationStorage).sortBy('name').value();}
-      };
-      //Connecting to the router
-      App.router.on('route:homeView', this.route, this);
-      //Scroll target: 
-      this._scrollTo = null;
+  /***/
+  initialize: function(){
+    this.model = {
+      studies: this.getAllStudyData
+    , siteLanguage: function(){return App.translationStorage.getBrowserMatch();}
+    , currentFlag: function(){return App.translationStorage.getFlag();}
+    , otherTranslations: function(){return _.chain(App.translationStorage.getOthers()).map(function(tId){
+        return {
+          tId: tId
+        , flag: this.getFlag(tId)
+        , name: this.getName(tId)
+        };
+      }, App.translationStorage).sortBy('name').value();}
+    };
+    //Connecting to the router
+    App.router.on('route:homeView', this.route, this);
+    //Scroll target: 
+    this._scrollTo = null;
+  }
+  /**
+    Activates callbacks to build parts of the model, which shall not be produced by /^update.+/ methods.
+  */
+  , activate: function(){
+      //Setting callbacks to update model:
+      App.translationStorage.on('change:translationId', this.buildStatic, this);
+      //Building statics:
+      this.buildStatic();
+    }
+    /**
+      Builds the static translations, and is, in contrast to /^update.+/ methods,
+      only called on activate and change of translationId.
+    */
+  , buildStatic: function(){
+      var staticT = App.translationStorage.translateStatic({
+          title: 'website_title_prefix'
+        , subtitle: 'website_title_suffix'
+        , funding: 'website_logo_hover'
+        , imprint: 'topmenu_about_imprint'
+        , imprintHref: 'topmenu_about_imprint_href'
+        , privacypolicy: 'topmenu_about_privacypolicy'
+        , privacypolicyHref: 'topmenu_about_privacypolicy_href'
+        , licenceText: 'topmenu_about_licencetext'
+        , licenceTextHref: 'topmenu_about_licencetext_href'
+      });
+      _.extend(this.model, staticT);
     }
     /**
     */
