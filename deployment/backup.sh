738045c9ed474f7f5fbed5fb65cab03d097616c8
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2018-06-12 12:38:17 +0200

    store backups for the last 100 days

--- deployment/backup.sh
+++ deployment/backup.sh
@@ -13,6 +13,6 @@ mysqldump -hlocalhost -usoundcomparisons -p$password --routines --single-transac
 echo "SET FOREIGN_KEY_CHECKS=1;" >> $name.sql
 echo "COMMIT;" >> $name.sql
 echo "SET AUTOCOMMIT=1;" >> $name.sql
-# Keeping only 10 latest dumps:
+# Keeping only 100 latest dumps:
 # Compare https://stackoverflow.com/a/10119963/448591
-ls -tr *sql | grep -v 'create\|dump.sql' | head -n -10 | xargs --no-run-if-empty rm
+ls -tr *sql | grep -v 'create\|dump.sql' | head -n -100 | xargs --no-run-if-empty rm
